ce1667d40409c47a0726fffcf615989312a6ea31
Patrick Hubers <phubers@users.noreply.github.com>
2017-05-07 19:03:19 +0200

    Added Erlang B

--- erlang.py
+++ erlang.py
@@ -5,16 +5,7 @@ Erlang functions for use in calculators
 
 @author: Patrick.Hubers
 '''
-from math import pow, factorial, exp, ceil, trunc
-
-def Poisson(actual, mean):
-    # naive:   math.exp(-mean) * mean**actual / factorial(actual)
-    # iterative, to keep the components from getting too large or small:
-    p = exp(-mean)
-    for i in xrange(actual):
-        p *= mean
-        p /= i+1
-    return p
+from math import exp, ceil, trunc
 
 def Workload(volume, avgHandleTime, intervalLength=60):
     return( (volume*avgHandleTime)/(60.0*intervalLength) )
@@ -22,12 +13,14 @@ def Workload(volume, avgHandleTime, intervalLength=60):
 def Occupancy(numAgents, workload):
     return( workload/(numAgents*1.0) )
 
+def ErlangB(numAgents, workload):
+    inverse_b = 1.0
+    for k in range(1, numAgents):
+        inverse_b = 1.0 + inverse_b * k / workload
+    return(1.0 / inverse_b)
+    
 def ErlangC(numAgents, workload):
-    pois_discrete = Poisson(numAgents, workload)
-    pois_cum = 0.0
-    for k in range (0, numAgents):
-        pois_cum += Poisson(k, workload)
-    erlangc = pois_discrete / (pois_discrete + (( 1.0 - Occupancy(numAgents,workload)) * pois_cum))
+    erlangc = numAgents * ErlangB(numAgents, workload) / (numAgents - workload * (1 - ErlangB(numAgents, workload)))
     return( erlangc )
 
 def AverageWaitingTime(numAgents, volume, avgHandleTime, intervalLength):
@@ -49,11 +42,12 @@ if __name__ == '__main__':
     vol = 400
     aht = 180
     wl = Workload(vol, aht, 60)
-    print 'Workload for %d calls/hour with an AHT of %d seconds is %d' % (vol, aht, wl)
+    print('Workload for %d calls/hour with an AHT of %d seconds is %d' % (vol, aht, wl))
     sgoal = 0.8
     stime = 20
     agents = AgentsForServiceLevel(vol, aht, 60, stime, sgoal)
-    print 'Required agents for a service level of %d/%d is %d' % (sgoal*100, stime, agents)
-    print 'Waiting chance for a caller is %f ' % ErlangC(agents, wl)
-    print 'Service level is %f ' % ServiceLevel(agents, vol, aht, 60, stime)
-    print 'Occupancy is %f' % Occupancy(agents, wl)
+    print('Required agents for a service level of %d/%d is %d' % (sgoal*100, stime, agents))
+    print('Waiting chance for a caller is %f ' % ErlangC(agents, wl))
+    print('Service level is %f ' % ServiceLevel(agents, vol, aht, 60, stime))
+    print('Occupancy is %f' % Occupancy(agents, wl))
+
